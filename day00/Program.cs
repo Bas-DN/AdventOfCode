a1966f35cbc38e7ebbd9b5d975abc5d779696867
BasW <BasW@Delta-N.nl>
2022-12-06 19:02:43 +0100

    day template

---
+++ day00/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace day00;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("STARTING");
+        FileStream fileStream = new FileStream("input.txt", FileMode.Open);
+        List<string> objects = new();
+        using (StreamReader reader = new StreamReader(fileStream))
+        {
+            String? line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (line.Length > 0)
+                {
+                }
+            }
+        }
+        Console.WriteLine($"");
+    }
+}
+public static class Utils
+{
+}
